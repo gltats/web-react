09029d84610d8342a90e34a4cfce60d1d5c99688
gltats <74324703+gltats@users.noreply.github.com>
2021-05-04 21:55:56 +0200

    1st steps
    web page basic idea

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import photo from './tati.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="d-flex h-100 text-center text-white bg-dark">
+      <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header className="mb-auto">
+    <div>
+      <h3 className="float-md-start mb-0">My website</h3>
+      <nav className="nav nav-masthead justify-content-center float-md-end">
+        <a className="nav-link active" aria-current="page" href="#">Home</a>
+        <a className="nav-link" href="#">About me</a>
+        <a className="nav-link" href="#">My work</a>
+        <a className="nav-link" href="#">Contact</a>
+      </nav>
+    </div>
+  </header>
+
+  <main className="px-3">
+    <h1>Tatiana Sofía Gomes</h1>
+    <p className="lead">Front-end developer based in Warsaw</p>
+    <p className="lead">
+    <img src={photo} className="photoImg" alt="photo"/>
+    </p>
+  </main>
+
+  <footer className="mt-auto text-white-50">
+    <p><a href="https://github.com/gltats/weather-react-app"
+         target="_blank">
+          Open-source code
         </a>
-      </header>
+          {" "}by Tatiana Gomes</p>
+  </footer>
+</div>
+
     </div>
   );
 }
