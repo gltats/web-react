ca9c07d9aaacacf18118522978a574485f3997e6
gltats <74324703+gltats@users.noreply.github.com>
2021-07-28 23:49:50 +0200

    about updated

--- src/About.js
+++ src/About.js
@@ -31,32 +31,31 @@ export default function About(){
                     
                    <div className="clearfix">
                         <img src={photo} className="col-md-6 float-md-start mb-3 ms-md-3" alt="me" id="photoImg"/>
+                        <br />
                         <div className="overlay">
                             <div className="myName">
+                           
                                 My name is Tatiana
                                 </div>
                         </div>
                         <p className="textDecription">
                             <br />
-                            <br />
-                            <br />
-                            <br />
-                            
-                       I'm a front-end developer with graphic design and photography edition skills originally from Spain, currently based 
+          
+                       I'm a front-end developer with graphic design and photography editing skills. I'm from Spain, currently based 
                        in Warsaw, Poland. 
                         </p>
                         <br />
                         <p>
-                        During my formal studies I also develop myself in my hobbies Photography and Edition, 
-                        learning the bases by my own and with the help of professional photographer, <a href="https://www.instagram.com/omd_photografy/" title="Osvaldo Photographer Link"
+                        During my formal studies I also develop myself in my hobbies Photography and Editing, 
+                        learning the basics by my own and with the help of a professional photographer, <a href="https://www.instagram.com/omd_photografy/" title="Osvaldo Photographer Link"
                             >@omd_photografy</a> .
                     </p>
                     </div>
                        <div className="row">
                             <div className="col">
-                            <p className="text"> I studied a Senior degree of Childhood Education, and during 
-                            my last study year, I decide to do Erasmus in Warsaw. Since there
-                            I´m in this beautiful city.
+                            <p className="text"> I studied a Senior degree of Childhood Education and during 
+                            my last year of studying, I decided to do Erasmus in Warsaw. Since then
+                            I live in this beautiful city.
                             <img src={erasmus} className="erasmus" alt="erasmus"/>
                             </p>
                             </div>
@@ -66,19 +65,25 @@ export default function About(){
                         </div>
                     
                     
-                    <p> When I was on my Erasmus I met IT students, and started to be interested
-                        on this field. I decided to give a try, and started to study by my
-                        own with the help of Youtube, Udemy, freeCodeCamp. As I felt it wasn´t enought
-                        I enroled on SheCodes Workshops which helped me a lot.
+                    <p> When I was in my Erasmus I met IT students and started to be interested
+                        in this field. I decided to give a try, and began to study by my
+                        own with the help of Youtube, Udemy, freeCodeCamp. At some point I felt it wasn´t enough, so I decided to
+                        enrole on SheCodes Workshops which helped me a lot.
                     </p>
                     <img src={introductiontoCodingCertificate} className="certificate" alt="certificate"/>
                     <img src={FrontEndDevelopmentCertificate} className="certificate" alt="certificate"/>
                     <img src={ReactDevelopmentCertificate} className="certificate" alt="certificate"/>
-                   
+                    
 
                     <p> 
-                        Currently I´m working for Huawei, as a Spanish Consultant and Manual Tester, and during my free
-                        time I enjoy to code my own projects! 
+                    <br />
+                        After my Erasmus I started to work as a teacher in <a href="https://www.sinfronteras.pl/" title="Sin Fronteras Language School"
+                            >Sin Fronteras</a> where I have the offer of design and layout the first version of a school book,  <a href="https://editnos.com/es/producto/arriba-2/" title="Spanish book, Arriba A2"
+                            >Arriba 2</a>. Since then I have been creating different designs and layouts using Adobe Illustrator and Adobe InDesign.
+                    </p>
+                    <p> 
+                        Currently I´m working at this school and also for Huawei as a Spanish Consultant and Manual Tester and during my free
+                        time I enjoy to code and design my own projects! 
                     </p>
                     <h4>
                     Technical skills:
